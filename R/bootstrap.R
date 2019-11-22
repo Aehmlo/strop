d3956cd1be7005287f8f420c6b02f10f895aba1b
Alex Hamilton <alex.hamilton@ou.edu>
2019-11-22 11:34:23 -0600

    First commit.
    Add bootstrap function skeleton.

---
+++ R/bootstrap.R
@@ -0,0 +1,13 @@
+#' Bootstrap constructor
+#'
+#' @param data The data from which to resample
+#' @param FUN The function of interest
+#' @param ... Additional arguments
+#'
+#' @return
+#' @export
+#'
+#' @examples
+bootstrap <- function(data, FUN = mean, ...) {
+    stop("Unimplemented")
+}
