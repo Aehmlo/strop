473edb866d36a958c8235554239cee578819d467
Alex Hamilton <alex.hamilton@ou.edu>
2019-11-22 12:05:28 -0600

    Make bootstrap constructor actually useful.
    Perform resamples, calculate sample and population values, and stash everything (including the function call) for later use, returning an object of class strop.

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -2,12 +2,27 @@
 #'
 #' @param data The data from which to resample
 #' @param FUN The function of interest
+#' @param n The number of resamples to perform
 #' @param ... Additional arguments
 #'
-#' @return
+#' @return A an object with class \code{strop}
 #' @export
 #'
 #' @examples
-bootstrap <- function(data, FUN = mean, ...) {
-    stop("Unimplemented")
+#' data(mtcars)
+#' bootstrap(mtcars$cyl)
+bootstrap <- function(data, FUN = mean, n = 1000, ...) {
+    call <- match.call()
+    fn <- match.fun(FUN)
+    pop <- fn(data)
+    samples <- matrix(sample(data, n * length(data), replace = T), ncol = n)
+    stats <- apply(samples, 2, FUN)
+    ret <- list(
+        call = call,
+        pop = pop,
+        samples = samples,
+        stats = stats
+    )
+    class(ret) <- append(class(ret), "strop")
+    invisible(ret)
 }
