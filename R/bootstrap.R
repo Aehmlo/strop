05e99f49d849882ccec6b86bf94b4e23ad133c84
Alex Hamilton <alex.hamilton@ou.edu>
2019-11-28 23:13:23 -0600

    Update bootstrap function (now a method).

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -5,24 +5,63 @@
 #' @param n The number of resamples to perform
 #' @param ... Additional arguments
 #'
+#' @return A bootstrap object of class \code{strop}
+#' @export
+#'
+bootstrap <- function(data, FUN, n, ...) UseMethod("bootstrap")
+
+#' #' Bootstrap constructor
+#'
+#' @param data The (atomic) data from which to resample
+#' @param FUN The function of interest
+#' @param n The number of resamples to perform
+#' @param ... Additional arguments
+#'
 #' @return A an object with class \code{strop}
 #' @export
 #'
 #' @examples
 #' data(mtcars)
 #' bootstrap(mtcars$cyl)
-bootstrap <- function(data, FUN = mean, n = 1000, ...) {
+bootstrap.default <- function(data, FUN = mean, n = 1000, ...) {
     call <- match.call()
     fn <- match.fun(FUN)
     pop <- fn(data)
     samples <- matrix(sample(data, n * length(data), replace = T), ncol = n)
-    stats <- apply(samples, 2, FUN)
-    ret <- list(
+    stats <- sapply(samples, FUN)
+    invisible(structure(class = "strop", list(
         call = call,
         pop = pop,
         samples = samples,
         stats = stats
-    )
-    class(ret) <- append(class(ret), "strop")
-    invisible(ret)
+    )))
+}
+
+#' Bootstrap constructor
+#'
+#' @param data The data frame from which to resample
+#' @param FUN The function of interest
+#' @param n The number of resamples to perform
+#' @param ... Additional arguments
+#'
+#' @return A an object with class \code{strop}
+#' @export
+#'
+#' @examples
+#' data(mtcars)
+#' bootstrap(mtcars, function(df) mean(df$cyl))
+bootstrap.data.frame <- function(data, FUN = mean, n = 1000, ...) {
+    call <- match.call()
+    fn <- match.fun(FUN)
+    pop <- fn(data)
+    samples <- lapply(1:n, function(.i) {
+        data[sample(1:nrow(data), nrow(data), replace = T),]
+    })
+    stats <- lapply(samples, fn)
+    invisible(structure(class = "strop", list(
+        call = call,
+        pop = pop,
+        samples = samples,
+        stats = stats
+    )))
 }
