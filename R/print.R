2fa32dcd24600f6dac4931985fb281431b1cc74d
Alex Hamilton <alex.hamilton@ou.edu>
2019-11-28 23:14:03 -0600

    Update print function for multiple statistics.

---
+++ R/print.R
@@ -0,0 +1,26 @@
+#' Bootstrap result printing
+#'
+#' @param x A boostrap object of class \code{strop}
+#' @param max The maximum number of bootstrap estimates to print
+#' @param ... Additional arguments
+#'
+#' @export
+#'
+#' @examples
+#' data(mtcars)
+#' print(bootstrap(mtcars$cyl))
+print.strop <- function(x, max = 10, ...) {
+    obj <- x
+    cl <- as.character(obj$call)
+    fn <- cl[1]
+    args <- cl[-1]
+    cat(paste("Call:", paste0(fn, "(", paste(args, collapse = ", "), ")"), "\n", sep = "\n"))
+    ests <- head(obj$stats, max)
+    cat("Sample estimates:\n")
+    print(obj$pop)
+    cat("\nBootstrap estimates:\n")
+    print(ests)
+    if(length(obj$stats) > max) {
+        cat(paste0("(", length(obj$stats) - max, " omitted)"))
+    }
+}
